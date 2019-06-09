d26f41799d67ade7342763bf1aaea7d0a2a5a6ef
DavidDierickx <dadie1@student.monash.edu>
2019-06-09 21:20:18 +1000

    Updated set_laser_power tests

--- piescope/lm/laser.py
+++ piescope/lm/laser.py
@@ -17,7 +17,6 @@ def set_laser_power(wavelength, power_percentage):
         raise TypeError('Wavelength must be an integer')
 
     if isinstance(power_percentage, int):
-        print(power_percentage)
         if (power_percentage <= 100) & (power_percentage >= 0):
             command = "(param-set! '" + laser + ":level " + str(
                 power_percentage) + ")\r"
@@ -25,5 +24,33 @@ def set_laser_power(wavelength, power_percentage):
             raise ValueError('Power percentage must be between 0 and 100')
     else:
         raise TypeError('Power percentage must be an integer')
+
+    return command
+
+
+def set_laser_enable(wavelength, onoff):
+    if isinstance(wavelength, int):
+        if wavelength == 405:
+            laser = "laser1"
+        elif wavelength == 488:
+            laser = "laser2"
+        elif wavelength == 561:
+            laser = "laser3"
+        elif wavelength == 640:
+            laser = "laser4"
+        else:
+            raise ValueError('Expecting a wavelength of 405, 488, 561 or 640')
+    else:
+        raise TypeError('Wavelength must be an integer')
+
+    if isinstance(onoff, str):
+        if onoff.lower == "on":
+            command = "(param-set! '" + laser + ":enable #t)\r"
+        elif onoff.lower == "off":
+            command = "(param-set! '" + laser + ":enable #f)\r"
+        else:
+            raise ValueError('On/off value must be "on" or "off"')
+    else:
+        raise TypeError('On/off value must be a string')
 
     return command
--- tests/test_lm/test_laser.py
+++ tests/test_lm/test_laser.py
@@ -2,57 +2,55 @@ import pytest
 from piescope.lm import laser
 
 
-def test_set_laser_power():
-    command = laser.set_laser_power(405, 50)
-    assert command == "(param-set! 'laser1:level 50)\r"
+@pytest.mark.parametrize("wavelength_pass, level_pass",
+                         [(405, 0), (488, 0), (561, 0), (640, 0),
+                          (405, 00), (488, 00), (561, 00), (640, 00),
+                          (405, 50), (488, 50), (561, 50), (640, 50),
+                          (405, 75), (488, 75), (561, 75), (640, 75),
+                          (405, 100), (488, 100), (561, 100), (640, 100)
+                          ],)
+def test_set_laser_power_pass(wavelength_pass, level_pass):
+    if wavelength_pass == 405:
+        las = "laser1"
+    elif wavelength_pass == 488:
+        las = "laser2"
+    elif wavelength_pass == 561:
+        las = "laser3"
+    elif wavelength_pass == 640:
+        las = "laser4"
 
-    command = laser.set_laser_power(405, 75)
-    assert command == "(param-set! 'laser1:level 75)\r"
+    cmd = laser.set_laser_power(wavelength_pass, level_pass)
 
-    command = laser.set_laser_power(488, 50)
-    assert command == "(param-set! 'laser2:level 50)\r"
+    assert cmd == "(param-set! '" + las + ":level " + str(level_pass) + ")\r"
 
-    command = laser.set_laser_power(488, 75)
-    assert command == "(param-set! 'laser2:level 75)\r"
 
-    command = laser.set_laser_power(561, 50)
-    assert command == "(param-set! 'laser3:level 50)\r"
-
-    command = laser.set_laser_power(561, 75)
-    assert command == "(param-set! 'laser3:level 75)\r"
-
-    command = laser.set_laser_power(640, 50)
-    assert command == "(param-set! 'laser4:level 50)\r"
-
-    command = laser.set_laser_power(640, 75)
-    assert command == "(param-set! 'laser4:level 75)\r"
-
-    with pytest.raises(ValueError):
-        laser.set_laser_power(405, 222)
-
+@pytest.mark.parametrize("wavelength_type_error, level_type_error",
+                         [(405, 22.2), (488, 22.2), (561, 22.2), (640, 22.2),
+                          (405, 50.0), (488, 50.0), (561, 50.0), (640, 50.0),
+                          (405, "50"), (488, "50"), (561, "50"), (640, "50"),
+                          (405.7, 0), (405.7, 50), (405.7, 75), (405.7, 100),
+                          (405.0, 0), (405.0, 50), (405.0, 75), (405.0, 100),
+                          ("405", 0), ("405", 50), ("405", 75), ("405", 100),
+                          ],)
+def test_set_laser_power_type(wavelength_type_error, level_type_error):
     with pytest.raises(TypeError):
-        laser.set_laser_power(405, 22.2)
-
-    with pytest.raises(TypeError):
-        laser.set_laser_power(405, 50.0)
-
-    with pytest.raises(TypeError):
-        laser.set_laser_power("405", 50)
-
-    with pytest.raises(ValueError):
-        laser.set_laser_power(420, 50)
-
-    with pytest.raises(TypeError):
-        laser.set_laser_power(405.7, 50)
-
-    with pytest.raises(TypeError):
-        laser.set_laser_power(405.0, 50)
-
-    with pytest.raises(TypeError):
-        laser.set_laser_power(405, "50")
+        laser.set_laser_power(wavelength_type_error, level_type_error)
 
     with pytest.raises(TypeError):
         laser.set_laser_power(504, 50, 1)
 
     with pytest.raises(TypeError):
         laser.set_laser_power(50)
+
+
+@pytest.mark.parametrize("wavelength_value_error, level_value_error",
+                         [(405, -1), (488, -1), (561, -1), (640, -1),
+                          (405, 101), (488, 101), (561, 101), (640, 101),
+                          (-1, 0), (-1, 50), (-1, 75), (-1, 100),
+                          (100, 0), (100, 50), (100, 75), (100, 100),
+                          (200, 0), (200, 50), (200, 75), (200, 100),
+                          (1000, 0), (1000, 50), (1000, 75), (1000, 100)
+                          ],)
+def test_set_laser_power_value(wavelength_value_error, level_value_error):
+    with pytest.raises(ValueError):
+        laser.set_laser_power(wavelength_value_error, level_value_error)
