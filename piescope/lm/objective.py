929b4f4c093c23945c4a93dfca3ce5eb10735bb9
DavidDierickx <dadie1@student.monash.edu>
2019-07-24 22:42:37 +1000

    Update stage controls

--- piescope/lm/objective.py
+++ piescope/lm/objective.py
@@ -2,11 +2,40 @@
 
 SMARACT stage hardware.
 """
+from socket import socket, AF_INET, SOCK_STREAM
+
+pre_string = ':'
+post_string = '\012'
 
 
-def move_absolute():
-    pass
+class StageController(socket):
+    def __init__(self, host='localhost', port=5000, timeout=2.0):
+        super().__init__(family=AF_INET, type=SOCK_STREAM)
+        self.settimeout(timeout)
+        try:
+            self.connect((host, port))
+        except Exception as error:
+            raise RuntimeError('Cannot connect to Smaract.'
+                               'Error: %s', error)
 
+    def move_absolute(self, position, hold=0):
+        cmd = pre_string + 'MPA0,' + str(position) + ',' + str(hold) + \
+              post_string
+        print(cmd)
+        ans = self.send_command(cmd)
+        return ans
 
-def move_relative():
-    pass
+    def move_relative(self, distance, hold=0):
+        cmd = pre_string + 'MPR0' + str(distance) + ',' + str(hold) + \
+              post_string
+        ans = self.send_command(cmd)
+        return ans
+
+    def current_position(self):
+        cmd = pre_string + 'GP0' + post_string
+        ans = self.send_command(cmd)
+        return ans
+
+    def send_command(self, cmd):
+        self.sendall(cmd)
+        return self.recv(1024)
