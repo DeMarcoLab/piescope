cb23c3d180690574fc0a679dc5a1e93a53984776
DavidDierickx <dadie1@student.monash.edu>
2019-07-25 14:55:50 +1000

    Adding detector file from detector branch as not in develop yet

---
+++ piescope/lm/detector.py
@@ -0,0 +1,45 @@
+"""Module for the Basler fluorescence detector."""
+
+from pypylon import pylon
+# from pypylon import genicam
+
+
+class Basler():
+    def __init__(self):
+        super(Basler, self).__init__()
+        self.camera = pylon.InstantCamera(
+            pylon.TlFactory.GetInstance().CreateFirstDevice())
+        print("Using device ", self.camera.GetDeviceInfo().GetModelName())
+        self.camera.Open()
+        # self.camera.ExposureTime = 1000
+        self.camera.MaxNumBuffer = 1
+        self.imageCount = 1
+        self.currentImageIndex = 0
+        self.imageList = []
+
+    def grab_frame(self):
+        self.camera.StartGrabbingMax(self.imageCount)
+        self.imageList = []
+        while self.camera.IsGrabbing():
+            grabResult = self.camera.RetrieveResult(
+                5000, pylon.TimeoutHandling_ThrowException)
+
+            if grabResult.GrabSucceeded():
+                print("SizeX: ", grabResult.Width)
+                print("SizeY: ", grabResult.Height)
+                img = grabResult.Array
+                self.imageList.append(img)
+                self.currentImageIndex = self.currentImageIndex + 1
+                print(img)
+                print("Gray value of first pixel: ", img[0, 0])
+                print(self.imageList)
+            else:
+                print("Error: ", grabResult.Errorcode,
+                      grabResult.ErrorDescription)
+            grabResult.Release()
+        return self.imageList
+
+
+# TODO: non-blocking live imaging
+# def live_image():
+#     pass
