ec94a12d00d29ec9af77b4cbd435e46d4150aac9
David Dierickx <Dadie1@student.monash.edu>
2019-08-07 16:45:49 +1000

    Fixed laser calls

--- piescope/lm/volume.py
+++ piescope/lm/volume.py
@@ -1,6 +1,7 @@
 from piescope.lm import objective, laser, detector
 import time
 
+
 # Assume minimum 0.5 microns per step maximum 300 microns total height
 
 time_delay = 1
@@ -12,43 +13,43 @@ def volume_acquisition(exposure_time, laser_list, laser_power_list, no_z_slices,
                        z_slice_distance):
 
     total_volume_height = (no_z_slices-1)*z_slice_distance
-    # print('Total height is: %s' % str(total_volume_height))
-    # print('Distance between slices is: %s' % str(z_slice_distance))
+    print('Total height is: %s' % str(total_volume_height) + '\n')
+    print('Distance between slices is: %s' % str(z_slice_distance) + '\n')
 
-    stage_controller = objective.StageController()
-    # # Only initialise if testing
-    stage_controller.initialise_system_parameters(0, 0, 0, 0)
-    # time.sleep(1)
-    # stage_controller.current_position()
+    # stage_controller = objective.StageController()
+    # stage_controller.initialise_system_parameters(0, 0, 0, 0)
 
-    # all_lasers = laser.initialize_lasers()
-    # # printing for testing:
-    # print('Lasers initialised')
+    lasers = laser.initialize_lasers()
+    print('Lasers successfully initialised \n')
 
-    # basler_detector = detector.Basler()
-    # print('Connected to detector')
+    basler_detector = detector.Basler()
+    print('Connected to detector \n')
 
-    initial_position = str(get_position(stage_controller))
-    print("Initial position is: %s" % initial_position)
+    # initial_position = str(get_position(stage_controller))
+    # print("Initial position is: %s \n" % initial_position)
 
-    stage_controller.move_relative(int(total_volume_height/2))
-
-    position = get_position(stage_controller)
-    print("Top of volume is located at: %s" % str(position))
+    # stage_controller.move_relative(int(total_volume_height/2))
+    # position = str(get_position(stage_controller))
+    # print("Top of volume is located at: %s \n" % position)
 
     for z_slice in range(1, no_z_slices):
 
         count = 0
 
         for i in range(0, len(laser_list)):
-            print('Implementing laser stuff in lab')
+            lasers[laser_list[i]].enable()
+            print([laser_list[i]])
 
-            # all_lasers[laser_list[i]].enable
-            # print('Laser %s is now enabled' % str(laser_list[i]))
+            l = lasers[laser_list[i]]
 
-            # all_lasers[laser_list[i]].set_laser_power
-            # print('Laser %s power is now %s' %
-            #       (str(laser_list[i], str(laser_power_list[i]))))
+            print(l.NAME)
+            print(l.WAVELENGTH)
+            print('Laser %s is now enabled' % str(laser_list[i]))
+
+            l.laser_power = laser_power_list[i]
+            print(laser_power_list[i])
+            print('Laser %s power is now %s' %
+                  (str(laser_list[i]), str(laser_power_list[i])))
 
             # all_lasers[laser_list[i]].emit
             # print('Laser %s now emitting' % str(laser_list[i]))
@@ -57,33 +58,32 @@ def volume_acquisition(exposure_time, laser_list, laser_power_list, no_z_slices,
 
             # # # save this image
 
-            # all_lasers[laser_list[i]].disable
-            # print('Laser %s now disabled' % str(laser_list[i]))
+            lasers[laser_list[i]].disable()
+            print('Laser %s now disabled' % str(laser_list[i]))
 
-        target_position = int(initial_position) + \
-            (int(total_volume_height/2)) - int((z_slice * z_slice_distance))
+        # target_position = int(initial_position) + \
+        #     (int(total_volume_height/2)) - int((z_slice * z_slice_distance))
         # print('Target position is: %s" % str(target_position))
 
-        stage_controller.move_relative(-z_slice_distance)
-        position = int(get_position(stage_controller))
+        # stage_controller.move_relative(-z_slice_distance)
+        # position = int(get_position(stage_controller))
 
-        difference = position - target_position
+        # difference = position - target_position
         # print('Difference is: %s' % str(difference))
 
-        while count < count_max and \
-                (difference > threshold or difference < -threshold):
+        # while count < count_max and \
+        #         (difference > threshold or difference < -threshold):
 
-            stage_controller.move_relative(-difference)
+            # stage_controller.move_relative(-difference)
 
-            position = int(get_position(stage_controller))
-            difference = position - target_position
+            # position = int(get_position(stage_controller))
+            # difference = position - target_position
             # print('Difference is: %s' % str(difference))
 
-            count = count + 1
+            # count = count + 1
             # print(count)
 
-    stage_controller.move_relative(int(total_volume_height/2))
-    # get_position(stage_controller)
+    # stage_controller.move_relative(int(total_volume_height/2))
 
 
 def get_position(stage):
@@ -92,5 +92,5 @@ def get_position(stage):
     return position
 
 
-# test acquisition
-volume_acquisition(1000, [1, 2, 4], [0, 100, 27], 30, 1000)
+volume_acquisition(1000, ['laser405', 'laser488', 'laser640'], [0.0, 30, 67], 3, 1000)
+
