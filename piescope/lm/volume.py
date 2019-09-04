5f2bc866605dede872ae56e85a1964c90cf29dca
DavidDierickx <dadie1@student.monash.edu>
2019-09-04 20:36:35 +1000

    Removed non-working default save path

--- piescope/lm/volume.py
+++ piescope/lm/volume.py
@@ -9,7 +9,7 @@ time_delay = 1
 count_max = 5
 threshold = 5
 
-DEFAULT_SAVE = 'C:\Users\Admin'
+# DEFAULT_SAVE = 'C:\Users\Admin'
 
 
 def volume_acquisition(exposure_time, laser_dict, no_z_slices,
@@ -20,68 +20,70 @@ def volume_acquisition(exposure_time, laser_dict, no_z_slices,
     print('Number of slices is: %s' % no_z_slices + '\n')
     print('Distance between slices is: %s' % z_slice_distance + '\n')
 
-    stage_controller = objective.StageController()
-    stage_controller.initialise_system_parameters(0, 0, 0, 0)
-    print('Stage controller successfully initialised \n')
-
-    lasers = laser.initialize_lasers()
-    print('Lasers successfully initialised \n')
-
-    basler_detector = detector.Basler()
-    print('Successfully connected to detector \n')
-
-    initial_position = str(get_position(stage_controller))
-    print("Initial position is: %s \n" % initial_position)
-
-    stage_controller.move_relative(int(total_volume_height/2))
-    position = str(get_position(stage_controller))
-    print("Top of volume is located at: %s \n" % position)
-
-    for z_slice in range(1, no_z_slices):
-
-        count = 0
-
-        for las, power in lasers.items():
-            lasers[las].enable()
-
-            print('%s is now enabled' % las)
-
-            lasers[las].laser_power = power
-            print(' %s power is %s' % (las, power))
-
-            lasers[las].emit()
-            print('%s now emitting' % las)
-
-            current_image = basler_detector.camera_grab()
-
-            inout.save_image(current_image, DEFAULT_SAVE)
-
-            lasers[laser].disable()
-            print('%s now disabled' % las)
-
-        target_position = int(initial_position) + \
-            (int(total_volume_height/2)) - int((z_slice * z_slice_distance))
-        print('Target position is: %s" % str(target_position))')
-
-        stage_controller.move_relative(-z_slice_distance)
-        position = int(get_position(stage_controller))
-
-        difference = position - target_position
-        print('Difference is: %s' % str(difference))
-
-        while count < count_max and \
-                (difference > threshold or difference < -threshold):
-            #
-            stage_controller.move_relative(-difference)
-            #
-            position = int(get_position(stage_controller))
-            difference = position - target_position
-            print('Difference is: %s' % str(difference))
-            #
-            count = count + 1
-            print(count)
-
-    stage_controller.move_relative(int(total_volume_height/2))
+    try:
+        stage_controller = objective.StageController()
+        stage_controller.initialise_system_parameters(0, 0, 0, 0)
+        print('Stage controller successfully initialised \n')
+    except:
+        print("Something went wrong")
+    # lasers = laser.initialize_lasers()
+    # print('Lasers successfully initialised \n')
+    #
+    # basler_detector = detector.Basler()
+    # print('Successfully connected to detector \n')
+    #
+    # initial_position = str(get_position(stage_controller))
+    # print("Initial position is: %s \n" % initial_position)
+    #
+    # stage_controller.move_relative(int(total_volume_height/2))
+    # position = str(get_position(stage_controller))
+    # print("Top of volume is located at: %s \n" % position)
+    #
+    # for z_slice in range(1, no_z_slices):
+    #
+    #     count = 0
+    #
+    #     for las, power in lasers.items():
+    #         lasers[las].enable()
+    #
+    #         print('%s is now enabled' % las)
+    #
+    #         lasers[las].laser_power = power
+    #         print(' %s power is %s' % (las, power))
+    #
+    #         lasers[las].emit()
+    #         print('%s now emitting' % las)
+    #
+    #         current_image = basler_detector.camera_grab()
+    #
+    #         inout.save_image(current_image, DEFAULT_SAVE)
+    #
+    #         lasers[laser].disable()
+    #         print('%s now disabled' % las)
+    #
+    #     target_position = int(initial_position) + \
+    #         (int(total_volume_height/2)) - int((z_slice * z_slice_distance))
+    #     print('Target position is: %s" % str(target_position))')
+    #
+    #     stage_controller.move_relative(-z_slice_distance)
+    #     position = int(get_position(stage_controller))
+    #
+    #     difference = position - target_position
+    #     print('Difference is: %s' % str(difference))
+    #
+    #     while count < count_max and \
+    #             (difference > threshold or difference < -threshold):
+    #         #
+    #         stage_controller.move_relative(-difference)
+    #         #
+    #         position = int(get_position(stage_controller))
+    #         difference = position - target_position
+    #         print('Difference is: %s' % str(difference))
+    #         #
+    #         count = count + 1
+    #         print(count)
+    #
+    # stage_controller.move_relative(int(total_volume_height/2))
 
 
 def get_position(stage):
