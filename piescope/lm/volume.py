c572a402e6f7a646b575b470a437a3e3663e28b0
DavidDierickx <dadie1@student.monash.edu>
2019-10-30 14:10:46 +1100

    Removed bare try excepts

--- piescope/lm/volume.py
+++ piescope/lm/volume.py
@@ -12,252 +12,102 @@ count_max = 5
 threshold = 5
 
 
-def volume_acquisition(self, exposure_time, laser_dict, no_z_slices,
+def volume_acquisition(main_gui, exposure_time, laser_dict, no_z_slices,
                        z_slice_distance):
 
-    try:
-        total_volume_height = (int(no_z_slices)-1)*int(z_slice_distance)
-        logger.debug('Total height is: %s' % str(total_volume_height) + '\n')
-        logger.debug('Number of slices is: %s' % no_z_slices + '\n')
-        logger.debug('Distance between slices is: %s' % z_slice_distance + '\n')
-    except Exception as e:
-        message = "Error in calculating total volume height.  Possibly due to"\
-                 " not having ints in z_slice_distance and no_z_slices fields"
-        logger.error(e)
-        logger.error(message)
-        return
-    try:
-        stage_controller = objective.StageController()
-    except Exception as e:
-        message = "Could not connect to stage controller"
-        logger.error(e)
-        logger.error(message)
-        return
+    total_volume_height = (int(no_z_slices)-1)*int(z_slice_distance)
+    logger.debug('Total height is: %s' % str(total_volume_height) + '\n')
+    logger.debug('Number of slices is: %s' % no_z_slices + '\n')
+    logger.debug('Distance between slices is: %s' % z_slice_distance + '\n')
 
-    try:
-        lasers = laser.initialize_lasers()
-        logger.debug('Lasers successfully initialised \n')
-    except Exception as e:
-        message = 'Could not initialise lasers'
-        logger.error(e)
-        logger.error(message)
-        return
+    stage_controller = objective.StageController()
 
-    try:
-        basler_detector = detector.Basler()
-        logger.debug('Successfully connected to detector \n')
-    except Exception as e:
-        message = 'Could not connect to Basler detector'
-        logger.error(e)
-        logger.error(message)
-        return
+    lasers = laser.initialize_lasers()
+    logger.debug('Lasers successfully initialised \n')
 
-    try:
-        initial_position = str(get_position(stage_controller))
-        logger.debug("Initial position is: %s \n" % initial_position)
-    except Exception as e:
-        message = 'Could not find initial position of stage'
-        logger.error(e)
-        logger.error(message)
-        return
+    basler_detector = detector.Basler()
+    logger.debug('Successfully connected to detector \n')
 
-    try:
-        stage_controller.move_relative(int(total_volume_height/2))
-        print('Moved to top of volume')
-    except Exception as e:
-        message = 'Could not move stage to top of volume'
-        logger.error(e)
-        logger.error(message)
-        return
+    initial_position = str(get_position(stage_controller))
+    logger.debug("Initial position is: %s \n" % initial_position)
 
-    try:
-        position = str(get_position(stage_controller))
-        print("Top of volume is located at: %s \n" % position)
-        time.sleep(time_delay)
-    except Exception as e:
-        message = 'Could not find top of volume position'
-        logger.error(e)
-        logger.error(message)
-        return
+    stage_controller.move_relative(int(total_volume_height/2))
+    print('Moved to top of volume')
 
-    try:
-        loop_range = int(no_z_slices)
-        print("Loop range is: {}".format(loop_range))
-    except ValueError:
-        message = 'no_z_slices incorrect type, please enter an int'
-        logger.error(e)
-        logger.error(message)
-        return
+    position = str(get_position(stage_controller))
+    print("Top of volume is located at: %s \n" % position)
+    time.sleep(time_delay)
 
-    try:
-        for z_slice in range(0, loop_range):
-            count = 0
-            for las, power in laser_dict.items():
-                print("laser: {}".format(las))
-                print("power: {}".format(power))
-                try:
-                    lasers[las].enable()
-                    print('%s is now enabled' % las)
-                except Exception as e:
-                    message = 'Could not enable %s' % las
-                    logger.error(e)
-                    logger.error(message)
-                    return
+    loop_range = int(no_z_slices)
+    print("Loop range is: {}".format(loop_range))
 
-                try:
-                    lasers[las].laser_power = int(power)
-                    print(' %s power is %s' % (las, power))
-                except Exception as e:
-                    message = 'Could not change %s power' % las
-                    logger.error(e)
-                    logger.error(message)
-                    return
+    for z_slice in range(0, loop_range):
+        count = 0
+        for las, power in laser_dict.items():
+            print("laser: {}".format(las))
+            print("power: {}".format(power))
+            lasers[las].enable()
+            print('%s is now enabled' % las)
 
-                try:
-                    lasers[las].emission_on()
-                    logger.debug('%s now emitting' % las)
-                except Exception as e:
-                    message = 'Could not emit %s' % las
-                    logger.error(e)
-                    logger.error(message)
-                    return
+            lasers[las].laser_power = int(power)
+            print(' %s power is %s' % (las, power))
 
-                try:
-                    if las == 'laser561':
-                        basler_detector.camera.ExposureTime.SetValue(125000)
-                        logger.debug('Exposure time is: {}'.format(
-                            basler_detector.camera.ExposureTime.GetValue()))
-                    else:
-                        basler_detector.camera.ExposureTime.SetValue(20000)
-                        message = 'Exposure time is: {}'.format(
-                            basler_detector.camera.ExposureTime.GetValue())
-                        logger.debug(message)
-                        self.current_image = basler_detector.camera_grab()
-                    self.array_list = self.current_image
-                except Exception as e:
-                    message = 'Could not grab basler image'
-                    logger.error(e)
-                    logger.error(message)
-                    return
+            lasers[las].emission_on()
+            logger.debug('%s now emitting' % las)
 
-                try:
-                    lasers[las].emission_off()
-                    time.sleep(1)
-                    logger.debug('%s stopped emitting' % las)
-                except Exception as e:
-                    message = 'Could not emit %s' % las
-                    logger.error(e)
-                    logger.error(message)
-                    return
+            if las == 'laser561':
+                basler_detector.camera.ExposureTime.SetValue(exposure_time)
+                logger.debug('Exposure time is: {}'.format(
+                    basler_detector.camera.ExposureTime.GetValue()))
+            else:
+                basler_detector.camera.ExposureTime.SetValue(exposure_time)
+                message = 'Exposure time is: {}'.format(
+                    basler_detector.camera.ExposureTime.GetValue())
+                logger.debug(message)
+                main_gui.current_image = basler_detector.camera_grab()
+            main_gui.array_list = main_gui.current_image
 
-                try:
-                    self.string_list = ['Volume_image_' + str(z_slice+1)
-                                        + '_of_' + str(loop_range) + las]
-                    self.update_display()
-                except Exception as e:
-                    message = 'Could not update display'
-                    logger.error(e)
-                    logger.error(message)
-                    return
+            lasers[las].emission_off()
+            time.sleep(1)
+            logger.debug('%s stopped emitting' % las)
 
-                try:
-                    self.save_image()
-                except Exception as e:
-                    message = 'Could not save image'
-                    logger.error(e)
-                    logger.error(message)
-                    return
+            main_gui.string_list = ['Volume_image_' + str(z_slice+1)
+                                + '_of_' + str(loop_range) + las]
+            main_gui.update_display()
 
-                try:
-                    lasers[las].disable()
-                    print('%s now disabled' % las)
-                except Exception as e:
-                    message = 'Could not disable %s' % las
-                    logger.error(e)
-                    logger.error(message)
-                    return
+            main_gui.save_image()
 
-            try:
-                target_position = float(total_volume_height/2.) \
-                                  + float(initial_position) \
-                                  - (float(z_slice) * float(z_slice_distance))
-                logger.debug('Initial position is: {}'.format(initial_position))
-                logger.debug('Z_slice is: {}'.format(z_slice))
-                logger.debug('z_slice_distance is: {}'.format(z_slice_distance))
-                logger.debug('Target position is: %s' % str(target_position))
-            except Exception as e:
-                message = 'Could not calculate target position'
-                logger.error(e)
-                logger.error(message)
-                return
+            lasers[las].disable()
+            print('%s now disabled' % las)
 
-            try:
-                stage_controller.move_relative(-int(z_slice_distance))
-            except Exception as e:
-                message = 'Failed to move stage controller by step distance'
-                logger.error(e)
-                logger.error(message)
-                return
+            target_position = float(total_volume_height/2.) \
+                              + float(initial_position) \
+                              - (float(z_slice) * float(z_slice_distance))
+            logger.debug('Initial position is: {}'.format(initial_position))
+            logger.debug('Z_slice is: {}'.format(z_slice))
+            logger.debug('z_slice_distance is: {}'.format(z_slice_distance))
+            logger.debug('Target position is: %s' % str(target_position))
 
-            try:
-                position = float(get_position(stage_controller))
-                logger.debug(position)
-            except Exception as e:
-                message = 'Failed to get current position after step movement'
-                logger.error(e)
-                logger.error(message)
-                return
+            stage_controller.move_relative(-int(z_slice_distance))
 
-            try:
-                difference = position - target_position
-                logger.debug('Difference is: %s' % str(difference))
-            except Exception as e:
-                message = 'Failed to find diff between desired and current pos'
-                logger.error(e)
-                logger.error(message)
-                return
+            position = float(get_position(stage_controller))
+            logger.debug(position)
 
-            while count < count_max and \
-                    (difference > threshold or difference < -threshold):
-                try:
-                    stage_controller.move_relative(-int(difference))
-                except Exception as e:
-                    message = 'Could not move controller by difference'
-                    logger.error(e)
-                    logger.error(message)
-                    return
+            difference = position - target_position
+            logger.debug('Difference is: %s' % str(difference))
 
-                try:
-                    position = float(get_position(stage_controller))
-                except Exception as e:
-                    message = 'Failed to retrieve position after correction'
-                    logger.error(e)
-                    logger.error(message)
-                    return
+        while count < count_max and \
+            (difference > threshold or difference < -threshold):
 
-                try:
-                    difference = position - target_position
-                    logger.debug('Difference is: %s' % str(difference))
-                except Exception:
-                    message = 'Failed to calculate difference after correction'
-                    logger.error(e)
-                    logger.error(message)
-                    return
+            stage_controller.move_relative(-int(difference))
 
-                count = count + 1
+            position = float(get_position(stage_controller))
+            difference = position - target_position
+            logger.debug('Difference is: %s' % str(difference))
 
-    except Exception as e:
-        logger.error(e)
-        logger.error("Laser loop error")
-        return
+            count = count + 1
 
-    try:
-        stage_controller.move_relative(int(total_volume_height/2))
-    except Exception as e:
-        message = 'Could not return stage to middle position'
-        logger.error(e)
-        logger.error(message)
-        return
+    stage_controller.move_relative(int(total_volume_height/2))
 
 
 def get_position(stage):
