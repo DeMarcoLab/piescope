e059a08472c5070352a381ce72cb392c2b175ac1
DavidDierickx <dadie1@student.monash.edu>
2019-11-05 05:53:19 +1100

    Added tif to rgba conversion function

--- piescope/utils.py
+++ piescope/utils.py
@@ -1,5 +1,14 @@
 import skimage.io as io
+from PIL import Image
+import numpy as np
 
 
 def save_image(image, dest):
     io.imsave(dest, image)
+
+
+def tif_conversion(image):
+    tif_image = Image.open(image)
+    rgba_image = np.array(np.flipud(tif_image.convert("RGBA")))
+
+    return rgba_image
