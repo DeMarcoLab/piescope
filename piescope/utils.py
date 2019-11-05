d1e168c8b5a4a1c8fe3011b75c1de3be56ebe83d
David Dierickx <Dadie1@student.monash.edu>
2019-11-05 19:21:03 +1100

    removed unecessary conversion function

--- piescope/utils.py
+++ piescope/utils.py
@@ -1,14 +1,5 @@
 import skimage.io as io
-from PIL import Image
-import numpy as np
 
 
 def save_image(image, dest):
     io.imsave(dest, image)
-
-
-def tif_conversion(image):
-    tif_image = Image.open(image)
-    rgba_image = np.array(np.flipud(tif_image.convert("RGBA")))
-
-    return rgba_image
