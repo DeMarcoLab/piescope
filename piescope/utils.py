79b7a8aed5204705db3f86c9e3916553e3413b31
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-11-11 17:43:34 +1100

    Add docstring to save_image() in utils

--- piescope/utils.py
+++ piescope/utils.py
@@ -1,10 +1,18 @@
 import skimage.io as io
 from autoscript_sdb_microscope_client.structures import AdornedImage
-import os.path as p
 
 
-def save_image(image, dest):
+def save_image(image, destination):
+    """Save image to file.
+
+    Parameters
+    ----------
+    image : ndarray or AdornedImage
+        Image array to save.
+    destination : str
+        Filename of saved image.
+    """
     if type(image) is AdornedImage:
-        image.save(dest)
+        image.save(destination)
     else:
-        io.imsave(dest, image)
+        io.imsave(destination, image)
