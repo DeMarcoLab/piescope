f368b495013d5b8d34035e39a3e7f7dfcef89a70
David Dierickx <Dadie1@student.monash.edu>
2019-11-11 12:41:56 +1100

    Removed unecessary prints

--- piescope/utils.py
+++ piescope/utils.py
@@ -5,8 +5,6 @@ import os.path as p
 
 def save_image(image, dest):
     if type(image) is AdornedImage:
-        print("ADORNED")
         image.save(dest)
     else:
-        print("Not Adorned")
         io.imsave(dest, image)
