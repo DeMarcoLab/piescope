0ecdd20254f02fa39d92b4f14de0ef190685a6f7
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2020-02-13 13:06:32 +1100

    Allow setup.py to install dependencies

--- piescope/__init__.py
+++ piescope/__init__.py
@@ -1,6 +1,1 @@
 from piescope._version import __version__
-
-import piescope.data
-import piescope.fibsem
-import piescope.lm
-import piescope.utils
--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
+import os
 from setuptools import setup, find_packages
 
 from piescope._version import __version__
+
+
+def parse_requirements_file(filename):
+    with open(filename) as fid:
+        requires = [l.strip() for l in fid.readlines() if l]
+
+    return requires
+
 
 descr = """DeMarco lab FIB-SEM package for automated controls."""
 
@@ -12,15 +21,18 @@ URL = 'https://github.com/DeMarcoLab/piescope'
 DOWNLOAD_URL = 'https://github.com/DeMarcoLab/piescope'
 VERSION = __version__
 PYTHON_VERSION = (3, 5)
-INST_DEPENDENCIES = []
+INST_DEPENDENCIES = parse_requirements_file(
+    os.path.join('requirements', 'default.txt')
+)
 
 if __name__ == '__main__':
-    setup(name=DISTNAME,
-          version=__version__,
-          url=URL,
-          description=DESCRIPTION,
-          long_description=LONG_DESCRIPTION,
-          author=MAINTAINER,
-          packages=find_packages(),
-          install_requires=INST_DEPENDENCIES
-          )
+    setup(
+        name=DISTNAME,
+        version=__version__,
+        url=URL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=MAINTAINER,
+        packages=find_packages(),
+        install_requires=INST_DEPENDENCIES,
+    )
