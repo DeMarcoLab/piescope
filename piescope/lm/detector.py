07952f6ccb25b44511a0b65c16aa03918a61ad7c
DavidDierickx <David.Dierickx1@monash.edu>
2021-03-03 14:04:57 +1100

    Added hardware triggering for camera_grab

--- piescope/lm/detector.py
+++ piescope/lm/detector.py
@@ -18,30 +18,46 @@ class Basler():
         self.currentImageIndex = 0
         self.image = []
 
-    def camera_grab(self, exposure_time=None, flip_image=True):
+    def camera_grab(self, exposure_time=None, trigger_mode='software', flip_image=True):
         """Grab a new image from the Basler detector.
 
         Parameters
         ----------
         exposure_time : int
             Exposure time, in microseconds (us).
+        trigger_mode : str
+            Trigger mode, possible values:
+            'hardware'
+            'software'
 
         Returns
         -------
         self.image : numpy array
         """
         self.camera.Open()
-        self.camera.ExposureMode.SetValue('Timed')
-        if exposure_time is not None:
-            try:
-                self.camera.ExposureTime.SetValue(float(exposure_time))
-            except Exception as e:
-                try:
-                    self.camera.ExposureTimeAbs.SetValue(float(exposure_time))
-                except Exception as e:
-                    self.camera.Close()
-                    raise e
+        self.camera.StopGrabbing()
 
+        if trigger_mode is 'software':
+            self.camera.TriggerMode.SetValue('Off')
+            self.camera.ExposureMode.SetValue('Timed')
+            if exposure_time is not None:
+                try:
+                    self.camera.ExposureTime.SetValue(float(exposure_time))
+                except Exception as e:
+                    try:
+                        self.camera.ExposureTimeAbs.SetValue(float(exposure_time))
+                    except Exception as e:
+                        self.camera.Close()
+                        raise e
+        elif trigger_mode is 'hardware':
+            self.camera.TriggerMode.SetValue('On')
+            self.camera.TriggerSource.SetValue('Line4')
+            self.camera.ExposureMode.SetValue('TriggerWidth')
+            self.camera.AcquisitionFrameRateEnable.SetValue(False)
+        else:
+            raise ValueError('Select "software" or "hardware"')
+
+        self.camera.StopGrabbing()
         self.camera.StartGrabbingMax(self.imageCount)
         self.image = []
 
